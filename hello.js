3d63cd15354271da7cfc1858d3500ab2ef20dd18
Oscar <omankiewicz77@gmail.com>
2023-01-10 11:43:09 -0500

    added a return function

--- hello.js
+++ hello.js
@@ -1,7 +1,23 @@
-const sayHello = function(name) {
+//function that say Hello to multiple people
+const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//function where output is shown in console right away
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('John');
+
+//return function where the variable must be called to be shown in console
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
