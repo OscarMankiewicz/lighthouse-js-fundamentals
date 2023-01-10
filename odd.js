9006df3b6b1effbeaa64e77bc85f70f700c5ea82
Oscar <omankiewicz77@gmail.com>
2023-01-10 12:15:11 -0500

    function that returns a value for odd or even numbers

---
+++ odd.js
@@ -0,0 +1,11 @@
+//function that determines if a number is odd or even
+const isOdd = function(num) {
+  if (num % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
