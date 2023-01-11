95abccb2b26b747810f5fbaae2e95bfbf90d12b9
Oscar <omankiewicz77@gmail.com>
2023-01-11 11:44:52 -0500

    ice cream challenge completed

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//adds root beer to the array & console logs the new array
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//console logs the first item in the array
+console.log(iceCreamFlavours[0]);
+
+//console logs the last item in the array
+console.log(iceCreamFlavours[5]);
+
+//console logs the length of the array
+console.log(iceCreamFlavours.length);
